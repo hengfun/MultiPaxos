6b06b3735dcfd7375888cebb2258901b2ab2a849
heng <hengfun@gmail.com>
2018-12-28 13:30:58 +0100

    clean up

--- MultiPaxos/run_2acceptor.sh
+++ MultiPaxos/run_2acceptor.sh
@@ -37,13 +37,13 @@ echo "starting proposers..."
 ./proposer.sh 2 $conf &
 
 echo "waiting to start clients"
-sleep 10
+sleep 1
 echo "starting clients..."
 
 ./client.sh 1 $conf < ../prop1 &
 ./client.sh 2 $conf < ../prop2 &
 
-sleep 5
+sleep 11
 
 $KILLCMD
 wait
